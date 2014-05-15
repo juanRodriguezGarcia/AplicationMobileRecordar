bf3efb2dfaf3cc501757cf040502415ae5d4c0ca
juanRodriguezGarcia <juangrodriguez10@gmail.com>
2014-05-14 21:38:31 -0500

    GrupoRecordarAplicationMobile

---
+++ jquery-mobile/scripts.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+$(document).ready(function () {
+	$('#formulario').submit(function() {
+		// recolecta los valores que inserto el usuario
+		var datosUsuario = $("#nombredeusuario").val()
+		var datosPassword = $("#clave").val()
+		archivoValidacion = "http://teledial.com.co/webservice/server2.php?jsoncallback=?"
+		 //alert(datosPassword)
+		$.getJSON(archivoValidacion, { usuario:datosUsuario ,password:datosPassword})
+		.done(function(respuestaServer) {
+		alert("\nGenerado en: " + respuestaServer.hora + "\n" +respuestaServer.generador+ "\n" +respuestaServer.respuesta+ "\n" +respuestaServer.validacion)
+				if(respuestaServer.validacion == "OK"){
+				/// si la validacion es correcta, muestra la pantalla "home"
+				alert("Datos validos");
+				$.mobile.changePage("#home")
+				}else{
+				/// ejecutar una conducta cuando la validacion falla
+				alert("datos invalidos");
+				}
+		})
+		return false;
+	})
+});
